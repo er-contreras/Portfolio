45651f715b3f0d8e7a876fd343d561b6256d4a85
unknown <jchernandez827@gmail.com>
2021-06-21 16:32:50 -0500

    Add script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+let menu = document.querySelector('.menu-dropdown')
+
+function openMenu() {
+    menu.style.visibility = 'visible';
+    console.log('HI')
+};
+
+function closeMenu() {
+    menu.style.visibility = 'hidden';
+};
