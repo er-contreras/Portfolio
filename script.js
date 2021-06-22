27852f1ec12572775d607cc38242bde4faf8c97f
Erick Contreras <eri89--@hotmail.com>
2021-06-22 18:46:02 -0500

    Add desktop popup window

--- script.js
+++ script.js
@@ -11,19 +11,33 @@ function closeMenu() {
 openMenu();
 closeMenu();
 
-const main = document.querySelector('.main-container')
+const main = document.querySelector('.main-container');
 const openModalButtons = document.querySelectorAll('[data-modal-target]');
 const closeModalButtons = document.querySelectorAll('[data-close-button]');
 const overlay = document.getElementById('overlay');
 
-openModalButtons.forEach(button => {
+function openModal(modal) {
+  if (modal === null) return;
+  modal.classList.add('active');
+  overlay.classList.add('active');
+  main.classList.add('active');
+}
+
+function closeModal(modal) {
+  if (modal === null) return;
+  modal.classList.remove('active');
+  overlay.classList.remove('active');
+  main.classList.remove('active');
+}
+
+openModalButtons.forEach((button) => {
   button.addEventListener('click', () => {
     const modal = document.querySelector(button.dataset.modalTarget);
     openModal(modal);
   });
 });
 
-closeModalButtons.forEach(button => {
+closeModalButtons.forEach((button) => {
   button.addEventListener('click', () => {
     // going to acces the parent modal of this button
     const modal = button.closest('.modal');
@@ -31,25 +45,9 @@ closeModalButtons.forEach(button => {
   });
 });
 
-function openModal(modal) {
-  if(modal === null) return
-  modal.classList.add('active');
-  overlay.classList.add('active');
-  main.classList.add('active');
-};
-
-function closeModal(modal) {
-  if(modal === null) return
-  modal.classList.remove('active');
-  overlay.classList.remove('active');
-  main.classList.remove('active');
-};
-
 overlay.addEventListener('click', () => {
   const modals = document.querySelectorAll('.modal.active');
-  modals.forEach(modal => {
+  modals.forEach((modal) => {
     closeModal(modal);
-  })
-})
-
-
+  });
+});
