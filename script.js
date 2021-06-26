b27c728a1c319afe333c0e61211af5f649f7deca
Erick Contreras <eri89--@hotmail.com>
2021-06-25 20:34:30 -0500

    Fix Linter errors

--- script.js
+++ script.js
@@ -552,12 +552,12 @@ const localStorageName = document.getElementById('name');
 const localStorageEmail = document.getElementById('email');
 const localStoragebtnInsert = document.getElementById('btnInsert');
 
-localStoragebtnInsert.onclick = function () {
+localStoragebtnInsert.onclick = () => {
   const key = localStorageName.value;
-  const value = localStorageEmail.value;
+  const { value } = localStorageEmail;
 
   if (key && value) {
     localStorage.setItem(key, value);
-    location.reload();
+    window.location.reload();
   }
 };
