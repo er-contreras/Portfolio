ee487a790c50f58c1dc3463932cf2c90443fbe54
Erick Contreras <eri89--@hotmail.com>
2021-06-25 20:19:02 -0500

    Add local storage

--- script.js
+++ script.js
@@ -546,3 +546,18 @@ form.addEventListener('submit', (event) => {
     event.preventDefault();
   }
 });
+
+// Local Storage ---------------------------------------->
+const localStorageName = document.getElementById('name');
+const localStorageEmail = document.getElementById('email');
+const localStoragebtnInsert = document.getElementById('btnInsert');
+
+localStoragebtnInsert.onclick = function () {
+  const key = localStorageName.value;
+  const value = localStorageEmail.value;
+
+  if (key && value) {
+    localStorage.setItem(key, value);
+    location.reload();
+  }
+};
