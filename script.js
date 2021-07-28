05818788a656fb28933f21a8f9c47b1cdea6f171
Erick Contreras <eri89--@hotmail.com>
2021-07-28 10:51:51 -0500

    update porfolio

--- script.js
+++ script.js
@@ -18,9 +18,9 @@ const projects = [
     descriptionMobile: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
     descriptionMobilePopup: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe.',
     descriptionDesktopPopUp: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scramble.',
-    image: './BG/Snapshoot-Portfolio.png',
+    image: 'todo-list.png',
     technologies: ['html', 'Ruby on rails', 'css'],
-    link: 'link_to_live_version.com',
+    link: 'https://eri8-9.github.io/todo-list/dist/',
   },
   {
     name: 'Multi - Post stories Two',
